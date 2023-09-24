f4a3ba2bac9acb8674c941383ca262dac16bc8e2
Maxime <sowlution.pro@gmail.com>
2023-09-24 03:14:17 +0200

    fix matching

--- app/matching/matching.tsx
+++ app/matching/matching.tsx
@@ -1,34 +1,36 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import './matching.css';
 import { useActiveProfile } from "@lens-protocol/react-web";
 import { getFilteredProfile } from '../api/callTaMere';
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
 
 export default function Matching() {
   const [profiles, setProfiles] = useState([]);
   const [currentProfileIndex, setCurrentProfileIndex] = useState(0);
   const { data: wallet, loading } = useActiveProfile();
-  const searchParams = useSearchParams()
-  const hack_name = searchParams.get('hack_name')
+  const searchParams = useSearchParams();
+  const hack_name = searchParams.get('hack_name');
+
+  // Fonction asynchrone pour charger les profils depuis l'API
+  const loadProfilesFromAPI = async () => {
+    try {
+      if (!loading && wallet?.handle !== undefined && hack_name !== null) {
+        const data = await getFilteredProfile(wallet?.handle ?? "", hack_name);
+        if (data !== null) {
+          setProfiles(data);
+          setCurrentProfileIndex(0); // Réinitialise l'index actuel
+        }
+      }
+    } catch (error) {
+      console.error('Erreur lors du chargement des profils :', error);
+    }
+  };
 
   useEffect(() => {
     // Chargez les profils depuis votre API au montage du composant
-    console.log(wallet?.handle, hack_name, loading)
-    if(!loading && wallet?.handle !== undefined && hack_name !== null && profiles.length === 0){
-      getFilteredProfile(wallet?.handle ?? "",hack_name)
-        .then((data) => {
-          console.log("data", data)
-          setProfiles(data.Social);
-        })
-        .catch((error) => {
-          console.error('Erreur lors du chargement des profils :', error);
-        });
-    }
-    console.log("profiles", profiles)
-    if(profiles.length >0){
-      console.log("profiles", profiles)
-    }
-  }, [loading, wallet?.handle,profiles]);
+    loadProfilesFromAPI(); // Appelez la fonction asynchrone ici
+  }, [loading, wallet?.handle]);
 
   const handleSwipeLeft = () => {
     // Enregistrez la décision de swipe à gauche (passer) via votre API si nécessaire
@@ -42,26 +44,7 @@ export default function Matching() {
     setCurrentProfileIndex(currentProfileIndex + 1);
   };
 
-  const loadNextProfile = () => {
-    // Si nous avons atteint la fin de la liste des profils, rechargez les profils depuis l'API
-    if (currentProfileIndex >= profiles.length - 1) {
-      getFilteredProfile(wallet?.handle ?? "",hack_name)
-        .then((data) => {
-          setProfiles(data.Social);
-          setCurrentProfileIndex(0);
-        })
-        .catch((error) => {
-          console.error('Erreur lors du chargement des profils :', error);
-        });
-    } else {
-      // Passez simplement au profil suivant
-      setCurrentProfileIndex(currentProfileIndex + 1);
-    }
-  };
-
   const currentProfile = profiles[currentProfileIndex];
-
- 
 
   return (
     <div className="main">
@@ -70,11 +53,11 @@ export default function Matching() {
         <div className="profile-card">
           {/* Affichez ici les informations du profil avec styles */}
           <div className="profile-image">
-            <img src={currentProfile.profileImage} alt={currentProfile.profileName} />
+            <img src={currentProfile.picture?.uri} alt={currentProfile.name} />
           </div>
-          <h1 className="profile-name">{currentProfile.profileName}</h1>
-          <p className="profile-bio">{currentProfile.profileBio}</p>
-          <p className="profile-display-name">{currentProfile.profileDisplayName}</p>
+          <h1 className="profile-name">{currentProfile.name}</h1>
+          <p className="profile-bio">{currentProfile.bio}</p>
+          <p className="profile-display-name">{currentProfile.handle}</p>
 
           {/* Boutons Swipe et Pass */}
           <div className="buttons">
@@ -87,7 +70,7 @@ export default function Matching() {
       )}
 
       {profiles.length > 0 && currentProfileIndex >= profiles.length && (
-        <button onClick={loadNextProfile} className="load-next-button">Load More Profile</button>
+        <button onClick={loadProfilesFromAPI} className="load-next-button">Load More Profile</button>
       )}
     </div>
   );
